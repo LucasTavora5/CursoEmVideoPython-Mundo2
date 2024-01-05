14397289dca5d6748d6e09fb7ef8595362f70846
Lucas Távora <lucastavora05@gmail.com>
2024-01-04 23:29:00 -0300

    Update Ex060.py

--- While/Ex060.py
+++ While/Ex060.py
@@ -1,3 +1,14 @@
 '''Exercício Python 060: Faça um programa que leia um número qualquer e mostre o seu fatorial. Exemplo:
 
 5! = 5 x 4 x 3 x 2 x 1 = 120'''
+
+n1 = int(input('Informe um número para saber seu fatorial: '))
+c = n1
+f = 1
+print(f'Calculando o fatorial de {n1}!=',end='')
+while c > 0:
+    print(f'{c} ',end='')
+    print('x ' if c > 1 else '= ', end='')
+    f *= c
+    c-=1
+print(f'{f}')
